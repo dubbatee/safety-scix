c372dc6cc27eae158c575da81125c87204367e90
dubbatee <157960144+dubbatee@users.noreply.github.com>
2024-02-06 23:09:59 +1100

    Update Results.py

--- Results.py
+++ Results.py
@@ -2,19 +2,44 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import statistics
-from SMC import meanSMCP
-from LMC import c, d
+from SMC import meanSMCP, meanSMCI, a, b
+from LMC import c, d, meanLMCI, meanLMCP
 
-#LMC derived relations
-print(c * meanSMCP + d)
+DistSMC = 62440
+DistLMC = 49590
 
-def LMCrel(meanlogP):
-  (c * meanlogP + d)
-  (
+#results from LMC PL relations
+
+def LMCdist(meanI, meanlogP):
+    var1 = ((np.float64(c) * np.float64(meanlogP)) + np.float64(d))
+    var2 = ((meanI) - var1) / 5
+    var3 = 10 * 10 ** var2
+    return(var3)
+
+#print(LMCdist(meanSMCI, meanSMCP))
+
+#results from SMC PL relations
+
+def SMCdist(meanI, meanlogP):
+    var1 = ((np.float64(a) * np.float64(meanlogP)) + np.float64(b))
+    var2 = ((meanI) - var1) / 5
+    var3 = 10 * 10 ** var2
+    return(var3)
 
 
-#SMC derived relations
+
+#results from GB PL relations
 
 
+#result % error
 
-#GB derived relations
+def SMCperror(dist):
+    print(dist)
+    print(((dist - 62440) / 62440)*100)
+
+def LMCperror(dist):
+    print(dist)
+    print(((dist - 49590) / 49590)*100)
+
+SMCperror(SMCdist(meanLMCI, meanLMCP))
+LMCperror(LMCdist(meanSMCI, meanSMCP))
