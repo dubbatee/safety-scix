5bbf4cc82c0a4453b6d07b88a79cbd78c10aae1c
dubbatee <157960144+dubbatee@users.noreply.github.com>
2024-02-23 23:35:50 +1100

    Update LMC.py

--- LMC.py
+++ LMC.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as mat
 import statistics
-
+from SMC import dfsmc, g, h
 
 
 DistLMC = 49590
@@ -17,11 +17,15 @@ def DMLMC(I):
 def LMCP(p):
     return(np.log10(p))
 
-absM = DMLMC(df['I'])
-logP = LMCP(df['P_1'])
+absMLMC = DMLMC(df['I'])
+logPLMC = LMCP(df['P_1'])
 
-meanLMCP = statistics.mean(logP)
+df['absM'] = absMLMC
+df['logP'] = logPLMC
+
+meanLMCP = statistics.mean(logPLMC)
 meanLMCI = statistics.mean(df['I'])
+
 
 #plotting values using matplotlib
 
@@ -40,3 +44,45 @@ mat.plot(x, LOBF, color = "red")
 mat.text(-0.8, 32, 'y = ' + format(c.round(2)) + 'x ' + "+ " + format(d.round(2)))
 mat.show()
 
+
+#bolometric correction calculations
+
+
+bc = 0
+
+def Mbol(M):
+    return (bc + M)
+
+df['Mbol'] = Mbol(df['absM'])
+
+def LMCmbol(Mbol):
+    return ((Mbol + (5*np.log10(DistLMC/10))))
+
+df['mbol'] = LMCmbol(df['Mbol'])
+
+#print(df['absM'])
+#print(df['Mbol'])
+#print(df['mbol'])
+
+z = ((DMLMC(df['mbol'])).tolist())
+w = ((LMCP(df['P_1'])).tolist())
+
+mat.scatter(w, z)
+e, f = np.polyfit(w, z, 1)
+mat.xlabel('Log10 of Period(day)')
+mat.ylabel('Abs I-band Mag')
+LOBF = [i * e + f for i in w]
+mat.plot(w, LOBF, color = "red")
+mat.text(-0.8, 32, 'y = ' + format(e.round(2)) + 'x ' + format(f.round(2)))
+mat.show()
+
+print(c, d)
+print(e, f)
+
+def bolodistlmc(mbol, logP):
+    var1 = (((g) * logP) + h)
+    var2 = ((mbol) - var1) / 5
+    var3 = 10 * 10 ** var2
+    return statistics.mean(var3)
+
+print(bolodistlmc(df['mbol'], LMCP(dfsmc['P_1'])))
